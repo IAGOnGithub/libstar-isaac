e34a0d1c4d2a0e50e943ab60bc9a9c0746ac4efd
IAGOnCodeberg <discorduseriamgod@gmail.com>
2023-06-27 01:02:08 -0400

    Add luaLibstar directory
    Previous commit somehow didn't include it

---
+++ luaLibstar/postUpdateCallback.lua
@@ -0,0 +1,19 @@
+local callback = {}
+
+local libstarCore = require("luaLibstar.libstarCore")
+
+function callback:Init(modRef)
+    modRef:AddCallback(ModCallbacks.MC_POST_UPDATE, function()
+        if      Input.IsActionPressed(ButtonAction.ACTION_SHOOTLEFT,  0) or
+                Input.IsActionPressed(ButtonAction.ACTION_SHOOTRIGHT, 0) or
+                Input.IsActionPressed(ButtonAction.ACTION_SHOOTUP,    0) or
+                Input.IsActionPressed(ButtonAction.ACTION_SHOOTDOWN,  0) then
+            
+            libstarCore:TrySpawn(Isaac.GetPlayer())
+        end
+        libstarCore:OnPostUpdate()
+    end)
+end
+
+return callback
+
